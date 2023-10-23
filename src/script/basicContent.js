6124121aa22701387121a13c18d74e15011be932
0000723669 <0000723669@senaimgaluno.com.br>
2023-10-23 18:40:36 -0300

    Trocando de perfil do aluno

---
+++ src/script/basicContent.js
@@ -0,0 +1,26 @@
+function header() {
+    document.querySelector("#header").innerHTML = `<header class="cardshadow">
+  <nav class="nav-items">
+      <a href="#inicio" class="button to-left "><img src="/src/images/profile.png" class="img" alt="Imagem de Perfil"></a>
+  
+      <a href="/src/../index.html#contato" class="button to-right padding-large">CONTATOS</a>
+      <a href="/src/../index.html#artigos" class="button to-right padding-large">ARTIGOS</a>
+      <a href="/src/../index.html#projetos" class="button to-right padding-large">PROJETOS</a>
+      <a href="/src/../index.html#sobre" class="button to-right padding-large">SOBRE</a>
+      
+  </nav>
+  </header>`
+}
+
+function footer() {
+    document.querySelector("#footer").innerHTML = `
+    <footer class="footer centervh">
+    <a href="https://github.com/jonathandopovo" target="_blank">
+        <p class="center opacity">&copy; Jonathan de Oliveira Rocha</p>
+    </a>
+  </footer>
+    `
+}
+    
+header()
+footer()
--- src/script/main.js
+++ src/script/main.js
@@ -1,27 +1,28 @@
 function openContent(event, nomeArtigo) {
-    var i, article_content, linkToArticle;
-    article_content = document.getElementsByClassName("article-content");
-    for (i = 0; i < article_content.length; i++) {
-      article_content[i].style.display = "none";
-    }
-    linkToArticle = document.getElementsByClassName("linkToArticle");
-    for (i = 0; i < linkToArticle.length; i++) {
-      linkToArticle[i].className = linkToArticle[i].className.replace(" active", "");
-    }
-    document.getElementById(nomeArtigo).style.display = "block";
-    event.currentTarget.className += " active";
+  var i, article_content, linkToArticle;
+  article_content = document.getElementsByClassName("article-content");
+  for (i = 0; i < article_content.length; i++) {
+    article_content[i].style.display = "none";
   }
-  
+  linkToArticle = document.getElementsByClassName("linkToArticle");
+  for (i = 0; i < linkToArticle.length; i++) {
+    linkToArticle[i].className = linkToArticle[i].className.replace(" active", "");
+  }
+  document.getElementById(nomeArtigo).style.display = "block";
+  event.currentTarget.className += " active";
+}
 
-  var url_atual = window.location.href;
-  var artigoPadrao=''
-  var a = url_atual.indexOf('#')
-  for(var i=a+1; i<url_atual.length;i++){
-    artigoPadrao+=url_atual[i]
-  }
-  if(artigoPadrao==url_atual){
-    document.getElementById('artigo1').click();
-  }else{
-    document.getElementById(`${artigoPadrao}`).click();
-  }
-  
+var url_atual = window.location.href;
+var artigoPadrao = ''
+var a = url_atual.indexOf('#')
+for (var i = a + 1; i < url_atual.length; i++) {
+  artigoPadrao += url_atual[i]
+}
+if (artigoPadrao == url_atual) {
+  document.getElementById('artigo1').click();
+} else {
+  document.getElementById(`${artigoPadrao}`).click();
+}
+
+
+
