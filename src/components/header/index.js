02f4e0fae1b952b287144aacea65d8042447261a
0000723669 <0000723669@senaimgaluno.com.br>
2023-10-23 21:55:44 -0300

    Fixing error

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 function header() {
     document.querySelector("#header").innerHTML = `<header class="cardshadow">
   <nav class="nav-items">
-      <a href="#inicio" class="button to-left "><img src="/src/images/profile.png" class="img" alt="Imagem de Perfil"></a>
+      <a href="/src/../index.html#inicio" class="button to-left "><img src="/src/images/profile.png" class="img" alt="Imagem de Perfil"></a>
   
       <a href="/src/../index.html#contato" class="button to-right padding-large">CONTATOS</a>
       <a href="/src/../index.html#artigos" class="button to-right padding-large">ARTIGOS</a>
