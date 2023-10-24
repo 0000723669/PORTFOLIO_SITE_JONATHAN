e83e71eafab9610e72c4bf7550888779722debee
0000723669 <0000723669@senaimgaluno.com.br>
2023-10-23 21:48:58 -0300

    Criando components

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,15 +12,4 @@ function header() {
   </header>`
 }
 
-function footer() {
-    document.querySelector("#footer").innerHTML = `
-    <footer class="footer centervh">
-    <a href="https://github.com/jonathandopovo" target="_blank">
-        <p class="center opacity">&copy; Jonathan de Oliveira Rocha</p>
-    </a>
-  </footer>
-    `
-}
-    
 header()
-footer()
